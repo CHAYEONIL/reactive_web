3e52cf4ab15739428d30d93b35cba8278061b4e5
CHAYEONIL <jeay123@naver.com>
2022-03-28 17:21:21 +0900

    [2022. 03. 28]

--- custom.js
+++ custom.js
@@ -1,5 +1,5 @@
 $(function(){
-// Trigger
+    // Trigger
     $('.trigger').click(function(){
         $(this).toggleClass('active')
         $('.gnb').toggleClass('active')
@@ -8,11 +8,11 @@ $(function(){
         $('.gnb').removeClass('active')
         $('.trigger').removeClass('active')
     })
-// Smooth Scrolling
+    // Smooth Scrolling
     $('.menu a').click(function(e) {
         $.scrollTo(this.hash || 0, 900)
     })
-// Change CSS with scroll
+    // Change CSS with scroll
     $(window).scroll(function() {
         if($(window).scrollTop() > 50) {
             $('header, .gototop').addClass('active')
@@ -20,6 +20,24 @@ $(function(){
         else {
             $('header, .gototop').removeClass('active')
         }
+    })
+    // Slick.js : history
+    $('.history-slider').slick({
+        dots: true,
+        infinite: false,
+        speed: 300,
+        slidesToShow: 4,
+        slidesToScroll: 2,
+    })
+    // Slick.js : project-photo
+    $('.project-photo').slick({
+        dots: true,
+        infinite: true,
+        speed: 500,
+        fade: true,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        cssEase: 'linear',
     })
 })
 
